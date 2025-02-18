cf243d8169edf0d1e4146f5ce9d17409b0c65f3a
Navin Kumar Verma <kumarnavinverma7@gmail.com>
2025-02-18 21:21:15 +0530

    Initialize Firebase and SharedPreferences; implement role-based navigation in main app entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:canteen/ManagerHomeScreen.dart';
 import 'package:canteen/StudentHomeScreen.dart';
+import 'package:canteen/loginscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,8 +17,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       debugShowCheckedModeBanner: false,
-      home: const StudentHomeScreen(),
+      title: 'canteen',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    home: FutureBuilder(
+      future: SharedPreferences.getInstance(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+      return CircularProgressIndicator();
+        } else {
+      final prefs = snapshot.data as SharedPreferences;
+      final role = prefs.getString('userRole');
+      if (role == 'student') {
+        return StudentHomeScreen();
+      } else if (role == 'manager') {
+        return ManagerHomeScreen();
+      } else {
+        return LoginScreen();
+      }
+        }
+      },
+    ),
     );
   }
 }
